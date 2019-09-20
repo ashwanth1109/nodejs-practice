2c8162995701b980e44614e92e2c550a45fb9cb8
Ashwanth A R <ashwanth1109@gmail.com>
2019-09-21 02:55:24 +0530

    Sample Getting Started

---
+++ 01_getting_started/sample.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const cities = require("cities");
+
+let myCity = cities.zip_lookup("10016");
+
+const { addTwoNum } = require("./addTwoNum");
+
+const sum = addTwoNum(2, 3);
+
+console.log(sum);
